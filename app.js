4f6a83cf1991eccb35c61e9a8865539e148cca3e
DIPESH <dipesh@DIPESHs-MacBook-Air.local>
2025-07-11 02:22:55 +0530

    app.js update3

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
