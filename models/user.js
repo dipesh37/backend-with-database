75d54f3f41212503b718843452aa5ac157b10460
DIPESH <dipesh@DIPESHs-MacBook-Air.local>
2025-07-16 01:08:19 +0530

    adding posts7

--- models/user.js
+++ models/user.js
@@ -9,4 +9,4 @@ const userSchema = mongoose.Schema({
   posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("user", userSchema);
